57511636ff3d259c19f361491abb523fdfdb428c
George A <sg1asgard@outlook.com>
2022-06-04 18:53:06 +0300

    initial setup

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,17 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.post("/api/data", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  return res.sendStatus(200);
 });
 
 app.listen(3000, () => {
