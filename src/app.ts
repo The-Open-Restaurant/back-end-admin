11c0d513badacfa4cb38a8c7906905f60e462995
George A <sg1asgard@outlook.com>
2022-06-04 18:21:34 +0300

    initial setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('The Open Restaurant')
+// v 1.0.0
+import express from "express";
+
+const app = express();
+
+app.get((req: Request, res: Response) => {
+  return res.send("From Express");
+});
+
+app.listen(3000, () => {
+  console.log("Application running on http://localhost:3000");
+});
