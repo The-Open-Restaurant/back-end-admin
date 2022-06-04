3e2c1fc3a2b6d5d70d8fa8408c1e78a1cc917a11
George A <sg1asgard@outlook.com>
2022-06-04 18:21:43 +0300

    initial setup

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 
 const app = express();
 
-app.get((req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   return res.send("From Express");
 });
 
