ad7709f2b27e1deb4034f7023c49f81fded145b8
George A <sg1asgard@outlook.com>
2022-06-10 20:07:08 +0300

    adding handlebars

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,19 @@
 // v 1.0.0
-import express, { Request, Response, NextFunction } from "express";
-// Consolidate is a Template engine consolidation library.
-// https://github.com/tj/consolidate.js
-import cons from "consolidate";
+const express = require("express");
+const routes = require("./routes/main");
 
 // Init Express
 const app = express();
-
-// Add Handlebars
-
-app.engine("handlebars", cons.handlebars);
-app.set("view engine", "handlebars");
-app.set("views", __dirname + "/views");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function (req: Request, res: Response) {
-  res.render("home", {layout: "main"});
-});
+// Add Handlebars
+app.set("view engine", "hbs");
+app.set("view options", { layout: false });
+app.set("views", __dirname + "/views");
+
+// Routes
+app.use('/', routes)
 
 app.listen(3000, () => {
   console.log("Application running on http://localhost:3000");
---
+++ src/routes/main.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req: Request, res: Response, next: NextFunction) {
+  res.render("home", {
+    layout: "layoutMain",
+    title: "Welcome to The Open Restaurant",
+    caption: "this totally works man!",
+  });
+});
+
+module.exports = router;
